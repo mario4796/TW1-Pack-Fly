8452e200a553447292c1d0744472f23576e4e737
arianezequiel01 <arbustamante@alumno.unlam.edu.ar>
2025-05-18 22:48:03 -0300

    commit this change

--- src/main/java/com/tallerwebi/presentacion/ControladorExcursion.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorExcursion.java
@@ -1,4 +1,14 @@
 package com.tallerwebi.presentacion;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ControladorExcursion {
+
+    @RequestMapping("/excursiones")
+    public ModelAndView irAExcursiones() {
+        return new ModelAndView("excursiones");
+    }
 }
--- src/test/java/com/tallerwebi/presentacion/ControladorExcursionTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorExcursionTest.java
@@ -1,4 +1,23 @@
 package com.tallerwebi.presentacion;
 
+import org.junit.Before;
+import org.testng.annotations.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.Assert.*;
+
 public class ControladorExcursionTest {
+
+    private ControladorExcursion controladorExcursion;
+
+    @Before
+    public void init() {
+        controladorExcursion = new ControladorExcursion();
+    }
+
+    @Test
+    public void queAlEntrarALaPaginaDeExcursionesSeMuestreLaVista() {
+        ModelAndView modelAndView = controladorExcursion.irAExcursiones();
+        assertEquals("excursiones", modelAndView.getViewName());
+    }
 }
