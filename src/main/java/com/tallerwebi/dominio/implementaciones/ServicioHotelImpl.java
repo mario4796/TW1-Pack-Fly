ad68913569ede021627bb614dc944334d2b4ed84
Lautaro <cl4utar0@gmail.com>
2025-06-18 12:50:57 -0300

    Centralizacion de las APIKEY

--- src/main/java/com/tallerwebi/dominio/implementaciones/ServicioHotelImpl.java
+++ src/main/java/com/tallerwebi/dominio/implementaciones/ServicioHotelImpl.java
@@ -1,5 +1,6 @@
 package com.tallerwebi.dominio.implementaciones;
 
+import com.tallerwebi.dominio.ConfiguracionDeApiKey;
 import com.tallerwebi.dominio.ServicioHotel;
 import com.tallerwebi.dominio.entidades.Hotel;
 import com.tallerwebi.dominio.entidades.HotelResponse;
@@ -14,18 +15,22 @@ import java.util.List;
 @Service
 @Transactional
 public class ServicioHotelImpl implements ServicioHotel {
-    private final String API_KEY = "cde27281bb7ca3316860cd43fb4d85c229615d4291c18787b26595b73bb92014";
+    private final ConfiguracionDeApiKey apiconfig;
     private final String gl = "ar"; //geolocalizacion ar = argentina
     private final String hl = "es"; //idioma es = español
     private final String currency = "ARS"; //formato de moneda ARS = pesos argentinos
 
     private RepositorioHotelImp repositorioHotelImp;
 
-    public ServicioHotelImpl(RepositorioHotelImp repositorioHotelImp) {
+    public ServicioHotelImpl(ConfiguracionDeApiKey apiconfig, RepositorioHotelImp repositorioHotelImp) {
+        this.apiconfig = apiconfig;
         this.repositorioHotelImp = repositorioHotelImp;
     }
 
     public List<HotelDto> buscarHoteles(String ciudad, String checkIn, String checkOut, Integer adults, Integer children, String children_ages) {
+
+        String API_KEY = apiconfig.getApiKey();
+
         RestTemplate restTemplate = new RestTemplate();
         String url = String.format(
                 "https://serpapi.com/search.json?engine=google_hotels&q=%s&check_in_date=%s&check_out_date=%s&adults=%d&children=%d&children_ages=%s&currency=%s&gl=%s&hl=%s&api_key=%s",
